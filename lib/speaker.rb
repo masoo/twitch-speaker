f3f9cec0f2c21c95db524dd3770702e389e44171
FUNABARA Masao <masao@masoo.jp>
2022-09-13 10:16:55 +0900

    fix constant

--- lib/speaker.rb
+++ lib/speaker.rb
@@ -3,6 +3,8 @@ require_relative "./mp3players/mpg123"
 
 # Class to speak voice.
 class Speaker
+  PATH="./vendor/mpg123.exe"
+
   # activate voice for AWS Polly
   def aws_activate(region:, access_key_id:, secret_access_key:)
     @client = Aws::Polly::Client.new(
@@ -10,7 +12,7 @@ class Speaker
       access_key_id: access_key_id,
       secret_access_key: secret_access_key
     )
-    @player = Mp3players::Mpg123.new(path: "./mpg123.exe")
+    @player = Mp3players::Mpg123.new(path: PATH)
   end
 
   # vocalize message
